1cdc3ff8523e6fa9cbc0129de5725597c9657983
matheusmaciell <matheus.sousa@ccc.ufcg.edu.br>
2019-11-26 21:56:51 -0300

    commite inicial

---
+++ src/main/java/treinamento/resource/CategoriaResource.java
@@ -0,0 +1,31 @@
+package treinamento.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import treinamento.model.Categoria;
+import treinamento.service.CategoriaService;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService categoriaService;
+	
+	@GetMapping("/listar")
+	public List<Categoria> listar(){
+		return categoriaService.listar();
+	}
+	
+	@PostMapping("/adicionar")
+	public void adicionar(@RequestBody Categoria categoria) {
+		categoriaService.adicionar(categoria);
+	}
+}
---
+++ src/main/java/treinamento/service/CategoriaService.java
@@ -0,0 +1,25 @@
+package treinamento.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import treinamento.model.Categoria;
+import treinamento.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public List<Categoria> listar(){
+		return categoriaRepository.findAll();
+	}
+
+	public void adicionar(Categoria categoria) {
+		categoriaRepository.save(categoria);
+		
+	}
+}
